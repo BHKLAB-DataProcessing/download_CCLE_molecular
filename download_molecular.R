d511ed669a205194ff5d55f2a0f02f1023d4abba
imanthonny <34114220+imanthonny@users.noreply.github.com>
2020-01-24 13:17:51 -0500

    Update download_molecular.R

--- download_molecular.R
+++ download_molecular.R
@@ -1,4 +1,4 @@
 library(downloader)
-basePath <- "https://orcestradata.blob.core.windows.net/ccle/CCLE/2015/"
+basePath <- "https://orcestradata.blob.core.windows.net/ccle/CCLE/2015"
 download(file.path(basePath, "CCLE_molecular.zip"), destfile="/pfs/out/CCLE_molecular.zip")
 unzip("/pfs/out/CCLE_molecular.zip", exdir="/pfs/out/CCLE_molecular")
